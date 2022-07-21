db207419f2b87d8e81c3f5eb753f62f1bfb637fd
lizhuo <1114865957@qq.com>
2022-07-21 17:42:01 +0800

    完成home页面开发

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 import Vue from 'vue'
 import App from './App'
+//导入网络请求
+import {$http} from '@escook/request-miniprogram'
+uni.$http=$http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
